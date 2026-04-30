7548f714ed3dd8f239eb11a9b211f214c227d5c3
João Arthur de Souza Resende <arthurjoao159@gmail.com>
2026-04-30 19:18:45 -0300

    corrigindo estrutura do projeto

--- src/main/java/com/nexus/jar/JarApplication.java
+++ src/main/java/com/nexus/jar/JarApplication.java
@@ -2,12 +2,13 @@ package com.nexus.jar;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.nexus.jar.repository")
 public class JarApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(JarApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(JarApplication.class, args);
+    }
 }
